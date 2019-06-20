58d9e421dfc0de690fe5a1044969f4f8bc72ed0d
nick <nickfarrell@gmail.com>
2019-06-20 13:30:03 -0700

    free and paid app flavors

--- app/src/paid/java/com/udacity/gradle/builditbigger/paid/MainActivityFragment.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/paid/MainActivityFragment.java
@@ -1,10 +1,12 @@
-package com.udacity.gradle.builditbigger;
+package com.udacity.gradle.builditbigger.paid;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.udacity.gradle.builditbigger.R;
 
 
 /**
@@ -18,7 +20,7 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_main, container, false);
+        View root = inflater.inflate(R.layout.fragment_main_paid, container, false);
 
 //        AdView mAdView = (AdView) root.findViewById(R.id.adView);
 //        // Create an ad request. Check logcat output for the hashed device ID to
